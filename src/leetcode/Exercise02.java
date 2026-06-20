3c20e3104a14033b91105f247fb86fc1c44ddf16
Vitoriano <henryquevitoriano@gmail.com>
2026-06-20 17:25:41 -0300

    Feat: Add Two Numbers

---
+++ src/leetcode/Exercise02.java
@@ -0,0 +1,22 @@
+package leetcode;
+
+import java.util.LinkedList;
+
+public class Exercise02 {
+    public static void main(String[] args) {
+        LinkedList<Integer> l1 = new LinkedList<>();
+        LinkedList<Integer> l2 = new LinkedList<>();
+
+        l1.add(2);
+        l1.add(4);
+        l1.add(3);
+
+        l2.add(5);
+        l2.add(6);
+        l2.add(4);
+    }
+
+    public static void Solution(LinkedList<Integer> l1, LinkedList<Integer> l2) {
+
+    }
+}
---
+++ src/resources/ListNode.java
@@ -0,0 +1,19 @@
+package resources;
+
+public class ListNode {
+    int val;
+    ListNode next;
+
+    public ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+
+}
