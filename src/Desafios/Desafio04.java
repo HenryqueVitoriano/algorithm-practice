f3ebddb7d87763da27e6e0da07a1b140ed450488
Vitoriano <henryquevitoriano@gmail.com>
2026-07-16 20:09:32 -0300

    Implementa Desafio04

--- src/Desafios/Desafio04.java
+++ src/Desafios/Desafio04.java
@@ -1,4 +1,16 @@
 package Desafios;
 
 public class Desafio04 {
+    /*
+    Dado um texto e uma lista de termos de busca (queries),
+    conte quantas vezes cada termo aparece no texto e
+    retorne os k termos com mais ocorrências.
+     */
+
+    static void main() {
+        String texto = "'";
+
+
+        System.out.println();
+    }
 }
